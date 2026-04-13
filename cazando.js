9fad039dcffd8cfb2a12781fbcc97e0e848ca7f6
Matiazz07 <damian.villota.2007y@gmail.com>
2026-04-13 15:17:37 -0500

    mover izquierda

--- cazando.js
+++ cazando.js
@@ -1,21 +1,19 @@
 let canvas=document.getElementById("areaJuego");
 let ctx=canvas.getContext("2d");
 
-let gatoX=0;
-let gatoY=0;
-let comidaX=0;
-let comidaY=0;
-
 const ALTO_GATO=80;
 const ANCHO_GATO=50;
 const ALTO_COMIDA=20;
 const ANCHO_COMIDA=20;
 
+let gatoX=canvas.width/2-(ANCHO_GATO/2);
+let gatoY=(canvas.height/2-ALTO_GATO/2);
+let comidaX=0;
+let comidaY=0;
+
 function graficarGato(){
-    gatoX = (canvas.width / 2) - (ANCHO_GATO / 2);
-    gatoY = (canvas.height / 2) - (ALTO_GATO/ 2);
-    let colorG= "#1900ff";
-    graficarRectangulo(gatoX, gatoY,ANCHO_GATO,ALTO_GATO, colorG);
+    let colorG= "#5005ff";
+    graficarRectangulo(gatoX,gatoY,ANCHO_GATO,ALTO_GATO,colorG);
 }
 
 function crearComida(){
@@ -30,4 +28,24 @@ function iniciarJuego(){
 function graficarRectangulo(x,y,ancho,alto,color){
     ctx.fillStyle = color;
     ctx.fillRect(x, y, ancho, alto);
+}
+
+function actualizarPantalla(){
+    limpiarCanvas();
+    graficarGato();
+    crearComida();
+}
+
+function moverIzquierda(){
+    gatoX=gatoX-10;
+    actualizarPantalla();
+}
+
+function moverDerecha(){
+    gatoX=gatoX+10;
+    actualizarPantalla();
+}
+
+function limpiarCanvas(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
 }
