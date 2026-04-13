83100bb628ab651bd17ec8bb2114459bff5e3130
Matiazz07 <damian.villota.2007y@gmail.com>
2026-04-13 00:03:31 -0500

    agregar graficarRectangulo

--- cazando.js
+++ cazando.js
@@ -1,7 +1,33 @@
 let canvas=document.getElementById("areaJuego");
 let ctx=canvas.getContext("2d");
 
+let gatoX=0;
+let gatoY=0;
+let comidaX=0;
+let comidaY=0;
+
+const ALTO_GATO=80;
+const ANCHO_GATO=50;
+const ALTO_COMIDA=20;
+const ANCHO_COMIDA=20;
+
 function graficarGato(){
-    ctx.fillStyle="blue"
-    ctx.fillRect(canvas.width/2 -25,canvas.height/2 -25,50,50)
+    gatoX = (canvas.width / 2) - (ANCHO_GATO / 2);
+    gatoY = (canvas.height / 2) - (ALTO_GATO/ 2);
+    let colorG= "#1900ff";
+    graficarRectangulo(gatoX, gatoY,ANCHO_GATO,ALTO_GATO, colorG);
+}
+
+function crearComida(){
+    graficarRectangulo(comidaX,comidaY,ANCHO_COMIDA,ALTO_COMIDA, "#FF0808");
+}
+
+function iniciarJuego(){
+    graficarGato();
+    crearComida();
+}
+
+function graficarRectangulo(x,y,ancho,alto,color){
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, ancho, alto);
 }
