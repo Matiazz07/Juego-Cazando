c86b64614c06b7ddf3415f1a4673f7fe0913dfca
Matiazz07 <damian.villota.2007y@gmail.com>
2026-04-20 20:44:09 -0500

    cuando gato come tiempo = 15

--- cazando.js
+++ cazando.js
@@ -11,7 +11,8 @@ let gatoY=(canvas.height/2-ALTO_GATO/2);
 let comidaX=0;
 let comidaY=0;
 let puntos=0;
-let tiempo=15;
+let tiempoMaximo=15;
+let tiempo=tiempoMaximo;
 let intervalo;
 
 function graficarGato(){
